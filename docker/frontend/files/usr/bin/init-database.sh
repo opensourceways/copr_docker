208cad5d14993159c84d7e2b6dbbdfc9f38c42b7
Li Chaoran <pkwarcraft@gmail.com>
2022-12-02 14:26:27 +0800

    upgrade frontend add aarch64
    Signed-off-by: Li Chaoran <pkwarcraft@gmail.com>

--- docker/frontend/files/usr/bin/init-database.sh
+++ docker/frontend/files/usr/bin/init-database.sh
@@ -2,4 +2,4 @@
 
 cd /usr/share/copr/coprs_frontend/ && copr-frontend create-db --alembic alembic.ini
 copr-frontend create-chroot \
-    $(ls /etc/mock/openeuler-*-{i386,x86_64}.cfg |xargs -I{} -n1 basename {} .cfg)
+    $(ls /etc/mock/openeuler-*-{i386,x86_64,aarch64}.cfg |xargs -I{} -n1 basename {} .cfg)
