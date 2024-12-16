17df7a82b48d209ca02908b837200f96b5fccdba
dakangsiji <50202209+dakangsiji@users.noreply.github.com>
2024-12-16 20:40:56 +0800

    Update init-script-for-startup.sh

--- docker/builder/init-script-for-startup.sh
+++ docker/builder/init-script-for-startup.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
+# Date: 2024-12
 
+# Install python pyporter application and git clone the latest copr_docker registry
 yes|cp -r -f /copr_docker/docker/builder/files/* /
+REPO_URL="https://gitee.com/openeuler/pyporter"
+TARGET_DIR="/pyporter"
 
 while true; do
-  git clone https://gitee.com/openeuler/pyporter /root/pyporter && cd /root/pyporter
-  python3 -m pip install -e .
-  if [ $? -eq 0 ];then echo "Install pyporter package Successed" >> /root/init-script-date.txt; break; fi
-  rm -rf /root/pyporter
+    if [ -d "$TARGET_DIR" ]; then
+        rm -rf "$TARGET_DIR"
+    fi
+
+    git clone $REPO_URL $TARGET_DIR
+    
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    sleep 1
 done
 
-rm -rf /copr_docker
-rm -rf /root/pyporter
+chown -R mockbuild. $TARGET_DIR
+
+while true
+do
+    su - mockbuild -c "python3 -m pip install -e $TARGET_DIR"
+    if [ $? -eq 0 ]; then 
+        echo "$(date +"%Y-%m-%d %H:%M:%S")  Install Package Successed" >> /tmp/init-script-data.txt
+        echo "$(date +"%Y-%m-%d %H:%M:%S")  Install Package Version:  $(su - mockbuild -c "pip list | grep $TARGET_DIR")" >> /tmp/init-script-data.txt
+        echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> /tmp/init-script-data.txt
+        break
+    fi
+done
+
+rm -rf  /copr_docker
