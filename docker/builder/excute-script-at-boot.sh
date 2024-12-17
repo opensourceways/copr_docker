c4943074c1367d5908537ce8603984a5b7e12575
dakangsiji <50202209+dakangsiji@users.noreply.github.com>
2024-12-17 09:25:03 +0800

    Create excute-script-at-boot.sh

---
+++ docker/builder/excute-script-at-boot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Date: 2024-12
+
+# Start executing script at boot, script path: /etc/rc.d/rc.local
+Copr_target_dir="/copr_docker"
+
+while true
+do
+    if [ -d "$Copr_target_dir" ]; then
+        rm -rf "$Copr_target_dir"
+    fi
+
+    git clone https://gh-proxy.test.osinfra.cn/https://github.com/opensourceways/copr_docker.git $Copr_target_dir
+      
+    if [ $? -eq 0 ];then
+        git -C $Copr_target_dir ls-remote --heads origin | grep main >> /tmp/init-script-data.txt
+        break
+    fi
+    sleep 1
+done
+
+bash $Copr_target_dir/docker/builder/init-script-for-startup.sh
