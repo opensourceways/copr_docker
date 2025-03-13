ca9d82a2ce5f3fec53a861c7584c7d0cc324467a
Coopermassaki <1277145053@qq.com>
2025-03-13 15:31:11 +0800

    support git-lfs

--- docker/builder/init-script-for-startup.sh
+++ docker/builder/init-script-for-startup.sh
@@ -5,6 +5,22 @@
 yes|cp -r -f /copr_docker/docker/builder/files/* /
 Pyporter_repo_url="https://gitee.com/openeuler/pyporter"
 
+# install git-lfs
+cat > /etc/yum.repos.d/eur2.repo <<EOF
+[copr:eur.openeuler.openatom.cn:fuyong666:eur]
+name=Copr repo for eur owned by fuyong666
+baseurl=https://eur.openeuler.openatom.cn/results/fuyong666/eur/openeuler-24.03_LTS_SP1-\$basearch/
+type=rpm-md
+skip_if_unavailable=True
+gpgcheck=1
+gpgkey=https://eur.openeuler.openatom.cn/results/fuyong666/eur/pubkey.gpg
+repo_gpgcheck=0
+enabled=1
+enabled_metadata=1
+EOF
+
+yum install -y git-lfs
+
 while true
 do
     python3 -m pip install git+$Pyporter_repo_url -i  https://mirrors.huaweicloud.com/repository/pypi/simple
